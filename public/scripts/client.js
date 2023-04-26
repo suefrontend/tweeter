9bb0f7bcba412579e31d1b5ca270f7d1b3e3c59a
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-25 23:09:46 -0700

    Create renderTweets function to render multiple tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,21 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
-  },
-  created_at: 1461116232227,
-};
-const createTweetElement = function (data) {
+const createTweetElement = function (tweetData) {
   return `
     <article class="tweet">
         <header>
             <div class="author">
-                <img src=${data.user.avatars} alt=${data.user.name} />
-                <span>${data.user.name}</span>
+                <img src=${tweetData.user.avatars} alt=${tweetData.user.name} />
+                <span>${tweetData.user.name}</span>
             </div>
-            <span class="additional-name">${data.user.handle}</span>
+            <span class="additional-name">${tweetData.user.handle}</span>
         </header>
         <div class="tweet-body">
-            <p>${data.content.text}</p>
+            <p>${tweetData.content.text}</p>
         </div>
         <footer>
-            <span>${data.created_at} days ago</span>
+            <span>${tweetData.created_at} days ago</span>
             <ul>
                 <li><i class="fa-sharp fa-solid fa-flag"></i></li>
                 <li><i class="fa-sharp fa-solid fa-retweet"></i></li>
@@ -39,8 +28,37 @@ const createTweetElement = function (data) {
   `;
 };
 
-$(document).ready(function () {
-  const $tweet = createTweetElement(tweetData);
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
-  $("#tweets-container").append($tweet);
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    $("#tweets-container").append(createTweetElement(tweet));
+  }
+};
+
+$(document).ready(function () {
+  renderTweets(data);
 });
