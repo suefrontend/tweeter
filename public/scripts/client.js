ce0dce3ddefe3c163a80df52b2d356cb704f2b22
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-25 22:05:09 -0700

    Create function to render tweets from data object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,44 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac",
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants",
+  },
+  created_at: 1461116232227,
+};
+const createTweetElement = function (data) {
+  return `
+    <article class="tweet">
+        <header>
+            <div class="author">
+                <img src=${data.user.avatars} alt=${data.user.name} />
+                <span>${data.user.name}</span>
+            </div>
+            <span class="additional-name">${data.user.handle}</span>
+        </header>
+        <div class="tweet-body">
+            <p>${data.content.text}</p>
+        </div>
+        <footer>
+            <span>10 days ago</span>
+            <ul>
+                <li><i class="fa-sharp fa-solid fa-flag"></i></li>
+                <li><i class="fa-sharp fa-solid fa-retweet"></i></li>
+                <li><i class="fa-sharp fa-solid fa-heart"></i></li>
+            </ul>
+        </footer>
+    </article>
+  `;
+};
 
+$(document).ready(function () {
+  const $tweet = createTweetElement(tweetData);
+
+  $("#tweets-container").append($tweet);
+});
